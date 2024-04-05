3679b31b4eb6708e8b7399b1c593e910f8d7aa30
kms0233 <105372558+kms0233@users.noreply.github.com>
2024-04-05 22:51:41 +0900

    Then 라이브러리 적용

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/WelcomeViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/WelcomeViewController.swift
@@ -7,49 +7,57 @@
 
 import Foundation
 import UIKit
+import Then
 
 class WelcomeViewController: UIViewController {
     
     private var id: String?
     
-    private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
-        imageView.image = UIImage(named:"puppy.png")
+    private let imageView = UIImageView()
+    private let welcomeLabel = UILabel()
+    private lazy var mainButton = UIButton()
+    private lazy var backToLoginButton = UIButton()
+    
+    func setStyle(){
+        //이미지
+        imageView.do{
+            $0.frame = CGRect(x: 112, y: 87, width: 150, height: 150)
+            $0.image = UIImage(named: "puppy.png")
+        }
         
-        return imageView
-    }()
+        //환영 텍스트
+        welcomeLabel.do{
+            $0.frame = CGRect(x: 140, y: 295, width: 95, height: 60)
+            $0.text = "???님\n반가워요!"
+            $0.textColor = .black
+            $0.textAlignment = .center
+            $0.numberOfLines = 2
+            $0.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+        }
+        
+        //메인으로 가는 버튼
+        mainButton.do{
+            $0.frame = CGRect(x: 20, y: 426, width: 335, height: 58)
+            $0.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+            $0.setTitle("메인으로", for: .normal)
+            $0.setTitleColor(.white, for: .normal)
+            $0.layer.cornerRadius = 6
+            $0.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        }
+        //로그인으로 되돌아가기 버튼
+        backToLoginButton.do{
+            $0.frame = CGRect(x: 20, y: 498, width: 335, height: 58)
+            $0.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+            $0.setTitle("다시 로그인", for: .normal)
+            $0.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
+            $0.layer.cornerRadius = 6
+            $0.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+            $0.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
+            
+        }
+    }
     
-    private let welcomeLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
-        label.text = "???님\n반가워요!"
-        label.textColor = .black
-        label.textAlignment = .center
-        label.numberOfLines = 2
-        label.font = UIFont(name: "Pretendard-ExtraBold", size:25)
-        return label
-    }()
-  
-    private lazy var mainButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 20, y: 426, width: 335, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
-        button.setTitle("메인으로", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size:18)
-        button.layer.cornerRadius = 6
-        return button
-    }()
-    
-    private lazy var backTologinButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 20, y: 498, width: 335, height: 58))
-        button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
-        button.setTitle("다시 로그인", for: .normal)
-        button.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
-        button.layer.cornerRadius = 6
-        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size:18)
-        button.addTarget(self, action: #selector(backToLoginButtonDidTap), for:.touchUpInside)
-        return button
-    }()
-    
+   
     
     //데이터 바인딩 함수 구현 -> 옵셔널 바인딩 사용
     private func bindID() {
@@ -57,6 +65,7 @@ class WelcomeViewController: UIViewController {
         self.welcomeLabel.text="\(idText)님 \n반가워요!"
     }
     
+    //외부에서도 접근할 수 있도록
     func setLabelText(id: String?) {
         self.id = id
     }
@@ -64,14 +73,16 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setStyle()
         self.view.backgroundColor = .white
-        [imageView, welcomeLabel, mainButton, backTologinButton].forEach{
+        [imageView, welcomeLabel, mainButton, backToLoginButton].forEach{
             self.view.addSubview($0)
         }
         
         bindID()
     }
     
+
     //이전 화면으로 가기
     @objc
     private func backToLoginButtonDidTap(){
