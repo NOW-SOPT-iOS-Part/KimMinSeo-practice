a32d0c4588f93b8db19eb006c557bfb0b5188567
kms0233 <cnblue789@naver.com>
2024-04-24 13:21:35 +0900

    [Chore] 레이아웃 수정

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/3주차/ItemCollectionViewCell.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/3주차/ItemCollectionViewCell.swift
@@ -12,48 +12,51 @@ final class ItemCollectionViewCell: UICollectionViewCell {
     var itemRow: Int?
     
     private let itemImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let priceLabel = UILabel()
+    private lazy var heartButton = UIButton()
     
-    private let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "333원"
-        label.textColor = .black
-        label.textAlignment = .center
-        label.font = UIFont(name: "Pretendard-Regular", size: 16)
-        return label
-    }()
-    
-    private let priceLabel: UILabel = {
-        let label = UILabel()
-        label.text = "333원"
-        label.textColor = .orange
-        label.textAlignment = .left
-        label.font = UIFont(name: "Pretendard-Bold", size: 15)
-        return label
-    }()
-    
-    private lazy var heartButton: UIButton = {
-        let button = UIButton()
-        button.setImage(.love, for: .normal)
-        button.setImage(UIImage(resource: .love2), for: .selected)
-        button.addTarget(self, action: #selector(heartButtonDidTap), for: .touchUpInside)
-        return button
-    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.backgroundColor = .white
         setLayout()
+        setStyle()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        
         self.heartButton.isSelected = false
     }
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    
+    
+    private func setStyle() {
+        nameLabel.do {
+            $0.text = "먀"
+            $0.textColor = .black
+            $0.textAlignment = .center
+            $0.font = UIFont(name: "Pretendard-Regular", size: 16)
+        }
+        
+        priceLabel.do {
+            $0.text = "333원"
+            $0.textColor = .orange
+            $0.textAlignment = .left
+            $0.font = UIFont(name: "Pretendard-Bold", size: 15)
+        }
+        
+        heartButton.do {
+            $0.setImage(.heartOff, for: .normal)
+            $0.setImage(UIImage(resource: .heartOn), for: .selected)
+            $0.addTarget(self, action: #selector(heartButtonDidTap), for: .touchUpInside)
+        }
     }
     
     private func setLayout() {
--- 34th-Sopt-Seminar/34th-Sopt-Seminar/3주차/ItemModel.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/3주차/ItemModel.swift
@@ -17,14 +17,14 @@ struct ItemModel {
 extension ItemModel {
     static func dummy() -> [ItemModel] {
         return [
-            ItemModel(itemImg: .character1, name: "배지현", price: "2,000원", heartIsSelected: false),
-            ItemModel(itemImg: .character2, name: "이현진", price: "5,000원", heartIsSelected: false),
-            ItemModel(itemImg: .character3, name: "오승연", price: "성북구", heartIsSelected: false),
-            ItemModel(itemImg: .character4, name: "이나경", price: "성북구", heartIsSelected: false),
-            ItemModel(itemImg: .character5, name: "이지민", price: "성북구", heartIsSelected: false),
-            ItemModel(itemImg: .character6, name: "류희재", price: "성북구", heartIsSelected: false),
-            ItemModel(itemImg: .character6, name: "류희재", price: "성북구", heartIsSelected: false),
-            ItemModel(itemImg: .character6, name: "류희재", price: "성북구", heartIsSelected: false)
+            ItemModel(itemImg: .character1, name: "케로로", price: "2,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character2, name: "기로로", price: "5,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character3, name: "타마마", price: "3,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character4, name: "도로로", price: "3,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character5, name: "푸루루", price: "3,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character6, name: "쿠루루", price: "3,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character6, name: "류희재", price: "3,000원", heartIsSelected: false),
+            ItemModel(itemImg: .character6, name: "류희재", price: "3,000원", heartIsSelected: false)
         ]
     }
 }
